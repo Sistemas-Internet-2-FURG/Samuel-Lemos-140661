1eb038f7ea421f01f1ffe17088a72498bb25c46f
Samuel Lemos <samueltdelemos@gmail.com>
2024-08-28 14:38:46 -0300

    feat: adicionando relação entre as tabelas course e user

--- Avaliações/Trabalhos/crud-flask/app/models/courseModel.py
+++ Avaliações/Trabalhos/crud-flask/app/models/courseModel.py
@@ -3,8 +3,8 @@ from app.config.db import db
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    addresses = db.relationship('Address', back_populates='cliente')
+    user = db.relationship('User', back_populates='courses')
 
     def __repr__(self):
-        return f'<Client {self.name}>'
+        return f'<Course {self.name}>'
     
--- Avaliações/Trabalhos/crud-flask/app/models/userModel.py
+++ Avaliações/Trabalhos/crud-flask/app/models/userModel.py
@@ -3,8 +3,9 @@ from app.config.db import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
-    addresses = db.relationship('Address', back_populates='cliente')
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    courses = db.relationship('Course', back_populates='user', cascade='all, delete-orphan')
 
     def __repr__(self):
-        return f'<Client {self.name}>'
+        return f'<User {self.name}>'
     
