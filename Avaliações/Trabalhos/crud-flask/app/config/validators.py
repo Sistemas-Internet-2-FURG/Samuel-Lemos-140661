cca953413a41637b1b0d6e0e1270634f6a2844c5
Samuel Lemos <samueltdelemos@gmail.com>
2024-08-28 15:01:55 -0300

    feat: adicionando password no validador e na model de user

--- Avaliações/Trabalhos/crud-flask/app/config/validators.py
+++ Avaliações/Trabalhos/crud-flask/app/config/validators.py
@@ -2,8 +2,7 @@ from marshmallow import Schema, fields, validates_schema, ValidationError, valid
 
 class UserSchema(Schema):
     name = fields.Str(required=True)
-    #email = fields.Email(required=True)
-    #password = fields.Str(required=True, validate=validate.Length(min=8))
+    password = fields.Str(required=True, validate=validate.Length(min=8))
 
 class CourseSchema(Schema):
     name = fields.Str(required=True)
--- Avaliações/Trabalhos/crud-flask/app/models/userModel.py
+++ Avaliações/Trabalhos/crud-flask/app/models/userModel.py
@@ -3,9 +3,9 @@ from app.config.db import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=True)
     courses = db.relationship('Course', back_populates='user', cascade='all, delete-orphan')
 
     def __repr__(self):
         return f'<User {self.name}>'
-    
