65bd2faf14f26b56e2b2cda6c127dd59b624ff9a
Samuel Lemos <samueltdelemos@gmail.com>
2024-08-28 18:17:08 -0300

    remove: removendo arquivos e iniciando novo modelo de estrutura de código

--- Avaliações/Trabalhos/crud-flask/app/config/validators.py
+++
@@ -1,8 +0,0 @@
-from marshmallow import Schema, fields, validates_schema, ValidationError, validate
-
-class UserSchema(Schema):
-    name = fields.Str(required=True)
-    password = fields.Str(required=True, validate=validate.Length(min=8))
-
-class CourseSchema(Schema):
-    name = fields.Str(required=True)
