a11f9f182f15e234a06ee682d77074fd67da7389
Samuel Lemos <samueltdelemos@gmail.com>
2024-08-29 11:53:57 -0300

    feat: adicionando relacionamento entra as tabelas user e car

--- Avaliações/Trabalhos/crud-flask/app/models.py
+++ Avaliações/Trabalhos/crud-flask/app/models.py
@@ -15,10 +15,11 @@ class Car(db.Model):
     year = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(255))
     type_id = db.Column(db.Integer, db.ForeignKey('car_types.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
-    
+    cars = db.relationship('Car', backref='user', lazy=True)
