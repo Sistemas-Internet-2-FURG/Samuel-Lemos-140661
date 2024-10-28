738c374ed252dd5141836b2c60500ae216e5e201
Samuel Lemos <samueltdelemos@gmail.com>
2024-10-28 15:41:52 -0300

    feat: refatorando a api e adicionando validação de usuário via token jwt

--- Avaliações/Trabalhos/Trabalho2-api-flask/api/app.py
+++ Avaliações/Trabalhos/Trabalho2-api-flask/api/app.py
@@ -3,9 +3,10 @@ from api.models import db, Car, CarType, User
 from api.config import Config
 from api.utils.logger import logger
 from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import os
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
@@ -13,94 +14,104 @@ db.init_app(app)
 def login(): #metodo para fazer login e autenticação do usuário
     logger.info("Iniciando serviço de login.")
 
-    if request.method == 'POST':
-        data = request.get_json()
-        name = data['name']
-        password = data['password']
+    data = request.get_json()
+    name = data.get("name")
+    password = data.get("password")
 
-        user = User.query.filter_by(name=name).first()
+    user = User.query.filter_by(name=name).first()
 
-        if user and (user.password == password): #verifica se a senha digitada está correta
-            session['user_id'] = user.id
-            session['name'] = user.name
-            return jsonify({
-                "message": "Usuário logado com sucesso.",
-                "user_id": user.id,
-                "user_name": user.name
-                }), 200
+    if not user or not user.check_password(password=password):
+        return jsonify({"message": "Credenciais inválidas!"}), 401
 
-@app.route('/logout')
-def logout():
-    session.pop('user_id', None)
-    session.pop('name', None)
+    token = create_access_token(identity=user.id)
 
-@app.route('/new_user', methods=['POST'])
-def new_user():
-    logger.info("Iniciando serviço de cadastro de novo usuário.")
-
-    if request.method == 'POST':
-        data = request.get_json()
-        name = data['name']
-        password = data['password']
-        new_user = User(name=name, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-        return jsonify({
-            "message": "Usuário cadastrado com sucesso.",
-            "user_id": new_user.id,
-            "user_name": new_user.name
+    return jsonify({
+        "message": "Usuário logado com sucesso.",
+        "token": token
         }), 200
 
-@app.route('/')
-def index(): #página inicial que mostra todos os veículos cadastrados 
-    # if 'user_id' not in session: #se o usuário não está logado redireciona para a página de login
-    #     return redirect(url_for('login'))
+@app.route('/register', methods=['POST'])
+def register():
+    logger.info("Iniciando serviço de cadastro de novo usuário.")
 
-    user_id = session['user_id']
-    cars = Car.query.filter_by(user_id=user_id).all()
-    car_types = CarType.query.all()
+    data = request.get_json()
+    name = data.get("name")
+    password = data.get("password")
+    new_user = User(name=name)
+    new_user.set_password(password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
     return jsonify({
-        "cars": cars,
-        "car_types": car_types
-    })
+        "message": "Usuário cadastrado com sucesso.",
+        "user_id": new_user.id,
+        "user_name": new_user.name
+    }), 201
+
+@app.route('/')
+@jwt_required()
+def index(): #página inicial que mostra todos os veículos cadastrados 
+    user_id = get_jwt_identity()
+    cars = Car.query.filter_by(user_id=user_id).all()
+
+    cars_response = []
+    for car in cars:
+        car_dict = {
+            "id": car.id,
+            "name": car.name,
+            "year": car.year,
+            "description": car.description,
+            "type_id": car.type_id,
+            "type_name": car.car_type.name,
+            "user_id": car.user_id
+        }
+        cars_response.append(car_dict)
+
+    return jsonify({
+        "cars": cars_response
+    }), 200
 
 @app.route('/new', methods=['POST'])
+@jwt_required()
 def new_car(): #adiciona um novo veículo
     logger.info('Iniciando serviço de cadastrar novo veículo.')
     
-    if request.method == 'POST':
-        data = request.get_json()
-        name = data['name']
-        year = data['year']
-        description = data['description']
-        type_id = data['type_id']
-        user_id = session['user_id']
-        new_car = Car(name=name, year=year, description=description, type_id=type_id, user_id=user_id)
-        db.session.add(new_car)
-        db.session.commit()
-        return jsonify({
-            "message": "Veículo cadastrado com sucesso.",
-            "user_id": user_id,
-            "car_name": name
-        }), 200
+    user_id = get_jwt_identity()
+    
+    data = request.get_json()
+    name = data.get("name")
+    year = data.get("year")
+    description = data.get("description")
+    type_id = data.get("type_id")
+
+    new_car = Car(name=name, year=year, description=description, type_id=type_id, user_id=user_id)
+    db.session.add(new_car)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Veículo cadastrado com sucesso.",
+        "user_id": user_id,
+        "car_name": name
+    }), 201
 
 @app.route('/edit/<int:id>', methods=['POST'])
 def edit_car(id): #edita o veículo com o id selecionado
     logger.info('Iniciando serviço de editar veículo.')
 
     car = Car.query.get_or_404(id)
-    if request.method == 'POST':
-        data = request.get_json()
-        car.name = data['name']
-        car.year = data['year']
-        car.description = data['description']
-        car.type_id = data['type_id']
-        db.session.commit()
-        return jsonify({
-            "message": "Veículo editado com sucesso.",
-            "user_id": car.user_id,
-            "car_name": car.name
-        }), 200
+
+    data = request.get_json()
+    car.name = data.get("name")
+    car.year = data.get("year")
+    car.description = data.get("description")
+    car.type_id = data.get("type_id")
+    db.session.commit()
+
+    return jsonify({
+        "message": "Veículo editado com sucesso.",
+        "user_id": car.user_id,
+        "car_name": car.name
+    }), 200
 
 @app.route('/delete/<int:id>')
 def delete_car(id): #deleta o veículo com o id selecionado
@@ -109,8 +120,9 @@ def delete_car(id): #deleta o veículo com o id selecionado
     car = Car.query.get_or_404(id)
     db.session.delete(car)
     db.session.commit()
+
     return jsonify({
-        "message": "Veículo excluído com sucesso.",
+        "message": "Veículo deletado com sucesso.",
         "user_id": car.user_id,
         "car_name": car.name
     }), 200
