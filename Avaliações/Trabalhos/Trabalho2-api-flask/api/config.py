94c15e8e0075714f06ef96c365dd972baa411b85
Samuel Lemos <samueltdelemos@gmail.com>
2024-10-28 15:25:34 -0300

    feat: adicionando config para token jwt e adicionando método para validação de password na model

--- Avaliações/Trabalhos/Trabalho2-api-flask/api/config.py
+++ Avaliações/Trabalhos/Trabalho2-api-flask/api/config.py
@@ -7,3 +7,4 @@ class Config: #configs do banco e orm
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://usuario:senha@localhost:5432/nome_do_banco')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'chave ultra secreta')
+    JWT_SECRET_KEY = os.getenv('SECRET_KEY', 'chave ultra secreta')
